dcd1ac2c305c3ead7b455995bfa8a447f7efa637
praveen1664 <praveen1664@outlook.com>
2023-01-24 12:37:14 +0530

    Final

--- UserService/src/test/java/com/jat/UserService/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/jat/UserService/UserServiceApplicationTests.java
@@ -7,21 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class UserServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-	@Autowired
-	private RatingService ratingService;
-
-	@Test
-	void createRating(){
-		Rating rating=Rating.builder().rating(10).userId("").feedback("This is generated from test class").hotelId("").build();
-		ResponseEntity<Rating> savedRating=ratingService.createRating(rating);
-
-		System.out.println("new rating Created"+savedRating);
-	}
-
-}
+//@SpringBootTest
+//class UserServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//	@Autowired
+//	private RatingService ratingService;
+//
+//	@Test
+//	void createRating(){
+//		Rating rating=Rating.builder().rating(10).userId("").feedback("This is generated from test class").hotelId("").build();
+//		ResponseEntity<Rating> savedRating=ratingService.createRating(rating);
+//
+//		System.out.println("new rating Created"+savedRating);
+//	}
+//
+//}
